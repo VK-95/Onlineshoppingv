c896200203aaeb2ad0229419f8301f17d95a9b85
VarunK <kvarun95@gmail.com>
2017-12-28 17:04:47 +0530

    My Project Setup

---
+++ Shoppingbackend/src/main/java/net/vrn/Shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.vrn.Shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
